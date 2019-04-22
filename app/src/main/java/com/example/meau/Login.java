f8c82a6f4bcedbaa73712c4f7112cc6422653d19
cesar.junior <cesar.junior@basis.com.br>
2019-04-22 15:43:14 -0300

    quase finalizado tela de cadastrar pessoa

--- app/src/main/java/com/example/meau/Login.java
+++ app/src/main/java/com/example/meau/Login.java
@@ -1,5 +1,6 @@
 package com.example.meau;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -26,4 +27,8 @@ public class Login extends AppCompatActivity {
         });*/
     }
 
+    public void cadastroPessoa(View view){
+        Intent it = new Intent(Login.this, CadastroPessoa.class);
+        startActivity(it);
+    }
 }
